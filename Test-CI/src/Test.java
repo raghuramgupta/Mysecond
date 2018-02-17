86abf2e97a06bc5e9d5cd7d7d184a9a914d94474
raghuramgupta <raghu_13_sa@yahoo.com>
2018-02-17 10:31:21 +0800

    Update Test.java

--- Test-CI/src/Test.java
+++ Test-CI/src/Test.java
@@ -1,24 +1,10 @@
 import java.io.*;
 public class Test {
 
-	public static void main(String[] args) throws IOException
+	public static void main(String[] args)
 	{
-		File f = new File("C:\\HDD\\Tools","Hello.txt");
-	      
-			FileWriter wr = new FileWriter(f,true);
-			wr.write(100);
-			wr.close();
-			System.out.println(f.length());
-			FileReader fr = new FileReader(f);
-			char[] a = new char[50];
-			fr.read();
-			String[] b = new String[1000];
-			int i=0;
-			for (char c:a)
-				{
-				System.out.println("Tesing the  "+c);
-				}
-			fr.close();
-		
+		System.Out.Println("This is a new project");
+		System.Out.Println("This is a new job");
+		System.Out.Println("This is a new change");
 	}
 }
